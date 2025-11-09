9d0aca0c8015c0b2de82b60d5b2a58dd454d8bf5
Freeman Wang <freemanwang2006@gmail.com>
2025-11-09 18:32:13 -0500

    Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -22,30 +22,41 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
+
         final StringBuilder result = new StringBuilder(
                 "Statement for " + getInvoice().getCustomer() + System.lineSeparator()
         );
+        for (Performance p : getInvoice().getPerformances()) {
+            result.append(String.format("  %s: %s (%s seats)%n",
+                    getPlay(p).getName(),
+                    usd(getAmount(p)),
+                    p.getAudience()));
+        }
 
+        result.append(String.format("Amount owed is %s%n",
+                usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
+        return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int totalAmount = 0;
+        for (Performance p : getInvoice().getPerformances()) {
+            totalAmount += getAmount(p);
+        }
+        return totalAmount;
+    }
+
+    private int getTotalVolumeCredits() {
+        int volumeCredits = 0;
         for (Performance p : getInvoice().getPerformances()) {
 
             // add volume credits
             volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
             volumeCredits += getVolumeCredits(p);
-
-            // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n",
-                    getPlay(p).getName(),
-                    usd(getAmount(p)),
-                    p.getAudience()));
-            totalAmount += getAmount(p);
         }
-        result.append(String.format("Amount owed is %s%n",
-                usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
-        return result.toString();
+        return volumeCredits;
     }
 
     private String usd(int amount) {
